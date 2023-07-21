7854b62feb1c4ee8ebabc1958e41c015879860ce
vikttur <105513387+vikttur@users.noreply.github.com>
2023-07-21 18:24:20 +0000

    Deploying to gh-pages from @ vikttur/goit-react-hw-08-phonebook@a8e61dff7fa186e68be5dbecab367ee46bf59b79 🚀

--- static/js/415.fa70a4f7.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var i=n(3329),r={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function c(){return(0,i.jsx)("div",{style:r.container,children:(0,i.jsxs)("h1",{style:r.title,children:["Task manager welcome page"," "]})})}}}]);
-//# sourceMappingURL=415.fa70a4f7.chunk.js.map
